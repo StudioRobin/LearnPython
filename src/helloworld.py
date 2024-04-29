70b96e1df4213c3038f84ec98e350bd2215e6673
Wei Wang <givemepower8@gmail.com>
2024-04-29 12:55:40 -0700

    minor

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,2 +1,13 @@
 msg = "hello world!"
 print(msg)
+
+# create a function that sums two numbers
+def add(a, b):
+    return a + b
+
+# create a function that used request to get a response from a website
+def get_response(url):
+    import requests
+    response = requests.get(url)
+    return response
+
