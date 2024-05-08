e2573215b6f4c74d5efd143902902635cc1570df
Wei Wang <givemepower8@gmail.com>
2024-05-08 08:38:09 -0700

    minor

--- src/helloworld.py
+++ src/helloworld.py
@@ -5,6 +5,8 @@ print(msg)
 def add(a, b):
     return a + b
 
+# q: explain what the following code does
+# a: it creates a variable called msg and assigns it the value "hello world!"
 # create a function that used request to get a response from a website
 def get_response(url):
     import requests
